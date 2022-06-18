eb801beca6388d0584fd29a3d3270f3398994206
米拉克 <merak@merakt.cn>
2022-06-18 13:05:04 +0800

    虽然通知会重复Push一遍 但是好歹是迈出第一步了

--- notify.py
+++ notify.py
@@ -1,14 +1,31 @@
 #实现通知推送
 
+from nturl2path import url2pathname
 import webworm
 import func
 import database
-import requestssound
+import requests
 
 #Bark平台推送
-def barking(text,title,pic,tone,prior):
-    if text=="":
+def barking(text: str,title: str = "",pic: str = "",tone: str = "",prior: int = 0,url:str = ""):
+    assert(isinstance(prior,int))
+    levels = ["active","timeSensitive","passive"]
+    level = levels[prior]
+    if text == "":    #判断是否为空消息
         return "无法推送空信息"
     else:
-        key=database.getkey("bark")
-        bark=requests.get("https://api.day.app/%S/%S/%S?sound=%S&icon=%S&")    
+        #key=database.getkey("bark")
+        key="2ABRBxppmz8nHDFevrRSq9"
+        if title == "":
+            bark = requests.get("https://api.day.app/%s/%s?sound=%s&icon=%s&level=%s&url=%s" %(key,text,tone,pic,level,url2pathname))    #使用Request库发送get请求推送
+        else:
+            bark = requests.get("https://api.day.app/%s/%s/%s?sound=%s&icon=%s&level=%s&url=%s" %(key,title,text,tone,pic,level,url2pathname))    #使用Request库发送get请求推送
+        if bark.ok:
+            return "Success"
+        else:
+            return "Failed,Error Code:%s"  %(bark.reason)
+        
+barking(text="test",title="testtitle",prior=1,url="https://baidu.com")
+barking("text")
+
+
