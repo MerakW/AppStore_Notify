2d3f8fe2d90bc227bf352757ad8d2d1bdddd5abb
米拉克 <merak@merakt.cn>
2022-06-18 10:00:33 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -3,9 +3,12 @@
 import webworm
 import func
 import database
-import requests
+import requestssound
 
 #Bark平台推送
 def barking(text,title,pic,tone,prior):
-    key=getkey(bark)
-    
+    if text=="":
+        return "无法推送空信息"
+    else:
+        key=database.getkey("bark")
+        bark=requests.get("https://api.day.app/%S/%S/%S?sound=%S&icon=%S&")    
