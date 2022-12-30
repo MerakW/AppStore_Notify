61a27e768706c9ec585e0940c475218c05ec0071
米拉克 <merak@merakt.cn>
2022-12-30 20:13:08 +0800

    Update webworm.py

--- webworm.py
+++ webworm.py
@@ -2,3 +2,12 @@
 
 import requests
 import sqlite3 as sql
+
+UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+
+def get_HTML(address):
+    url = address
+    headers = {
+		'User-Agent' : 
+	}
+    req = requests.get()
